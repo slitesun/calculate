64a8e4f9766d6f69bb4586739ef8a237ff1f098a
slitesun <34884186+slitesun@users.noreply.github.com>
2018-04-25 13:53:55 +0800

    Update app.d

--- src/app.d
+++ src/app.d
@@ -1,44 +1,50 @@
-import std.stdio;
-import std.conv;
-import std.string;
+import std.stdio:writeln, readln;
+import std.conv:parse;
+import std.string:chomp;
+import std.regex:matchAll;
  
 void main(){
   writeln("На данный момент поддерживается 4 типа операции: сложение, вычитание, умножение, деление. Введите тип операции");
   string nameOperation = readln.chomp;
-  writeln("Тип выбранной операции: ", nameOperation);
- 
-  writeln("Введите первое число");
-  string a = readln();
- 
-  writeln("Введите второе число");
-  string b = readln();
- 
-  //Преобразование переменных в тип double
-  auto first = parse!double(a);
-  auto second = parse!double(b);
- 
-  double result;
   
-  switch(nameOperation){
-    case "+":
-      result = first + second;
-      break;
+  if(nameOperation.matchAll("[-+*/]{1}")){
+    writeln("Тип выбранной операции: ", nameOperation);
+ 
+    writeln("Введите первое число");
+    string a = readln();
+ 
+    writeln("Введите второе число");
+    string b = readln();
+ 
+    //Преобразование переменных в тип double
+    auto first = parse!double(a);
+    auto second = parse!double(b);
+ 
+    double result;
+  
+    switch(nameOperation){
+      case "+":
+        result = first + second;
+        break;
    
-    case "-":
-      result = first - second;
-      break;
+      case "-":
+        result = first - second;
+        break;
    
-    case "*":
-      result = first * second;
-      break;
+      case "*":
+        result = first * second;
+        break;
    
-    case "/":
-      result = first / second;
-      break;
+      case "/":
+        result = first / second;
+        break;
      
-    default:
-      writeln("Неверно выбран тип операции.");
-      break;
+      default:
+        writeln("Неверно выбран тип операции.");
+        break;
+      }
+    writeln(result);
+  } else {
+      writeln("Неизвестный тип операции: ", nameOperation);
     }
-  writeln(result);
 }
