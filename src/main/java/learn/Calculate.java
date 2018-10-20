fceb63a4e4caed24c5b8ef7584e5bfa39fc59862
slitesun <34884186+slitesun@users.noreply.github.com>
2018-10-20 19:05:16 +0800

    Update Calculate.java

--- src/main/java/learn/Calculate.java
+++ src/main/java/learn/Calculate.java
@@ -5,58 +5,47 @@ import java.util.Scanner;
 
 public class Calculate {
     public static void main(String args[]) {
-        Scanner operation = new Scanner(System.in);
-        Scanner firstNumber = new Scanner(System.in);
-        Scanner secondNumber = new Scanner(System.in);
+        System.out.println("На данный момент поддерживается 4 типа операции: сложение, вычитание, умножение, деление. Введите тип операции: ");
+        String nameOperation = new Scanner(System.in).nextLine();
+        if (!nameOperation.matches("^[-+*/]$")) {
+            System.out.println("Неизвестный тип операции: " + nameOperation);
+            System.exit(0);
+        }
+
         try {
-            System.out.println("На данный момент поддерживается 4 типа операции: сложение, вычитание, умножение, деление. Введите тип операции: ");
-            String nameOperation = operation.nextLine();
-            nameOperation = nameOperation.replaceAll("[^-+*/]", "");
+            System.out.println("Введите первое число: ");
+            BigDecimal firstBD = new BigDecimal(Double.parseDouble(new Scanner(System.in).nextLine()));
 
-            if (!nameOperation.matches("^[-+*/]$")) {
-                System.out.println("Неизвестный тип операции: " + nameOperation);
-            } else {
-                System.out.println("Введите первое число: ");
-                String a = firstNumber.nextLine();
-                double first = Double.parseDouble(a);
+            System.out.println("Введите второе число: ");
+            BigDecimal secondDB = new BigDecimal(Double.parseDouble(new Scanner(System.in).nextLine()));
 
+            BigDecimal resultDB = new BigDecimal(0);
 
-                System.out.println("Введите второе число: ");
-                String b = secondNumber.nextLine();
-                double second = Double.parseDouble(b);
+            switch (nameOperation) {
+                case "+":
+                    resultDB = firstBD.add(secondDB);
+                    break;
 
-                double result = 0;
+                case "-":
+                    resultDB = firstBD.subtract(secondDB);
+                    break;
 
-                BigDecimal firstBD = new BigDecimal(first);
-                BigDecimal secondDB = new BigDecimal(second);
-                BigDecimal resultDB = new BigDecimal(result);
+                case "*":
+                    resultDB = firstBD.multiply(secondDB);
+                    break;
 
-                switch (nameOperation) {
-                    case "+":
-                        resultDB = firstBD.add(secondDB);
-                        break;
+                case "/":
+                    resultDB = firstBD.divide(secondDB, 1, BigDecimal.ROUND_DOWN);
+                    break;
 
-                    case "-":
-                        resultDB = firstBD.subtract(secondDB);
-                        break;
-
-                    case "*":
-                        resultDB = firstBD.multiply(secondDB);
-                        break;
-
-                    case "/":
-                        resultDB = firstBD.divide(secondDB, 1, BigDecimal.ROUND_DOWN);
-                        break;
-
-                    default:
-                        break;
-                }
-                System.out.println("Ответ: " + resultDB.setScale(1, BigDecimal.ROUND_DOWN));
+                default:
+                    break;
             }
+            System.out.println("Ответ: " + resultDB.setScale(2, BigDecimal.ROUND_DOWN));
+        } catch (ArithmeticException e) {
+            System.out.println("Анус свой подели на ноль пёс");
         } catch (NumberFormatException e){
             System.out.println("Во вводе обнаружены недопустимые символы. Допустимы только числа");
-        } catch (ArithmeticException e){
-            System.out.println("Ошибка: " + e);
         }
     }
 }
